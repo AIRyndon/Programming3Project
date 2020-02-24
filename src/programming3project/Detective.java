d24ec786aaa1750cd44041a8cb1b6738b459e605
Angelo Ryndon <angeloryndon@outlook.com>
2020-02-25 11:19:32 +1300

    Initial commit

---
+++ src/programming3project/Detective.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programming3project;
+
+/**
+ *
+ * @author airyn
+ */
+public class Detective {
+    
+    private String name;
+    private String background;
+    
+    public Detective(){
+    
+        
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the background
+     */
+    public String getBackground() {
+        return background;
+    }
+
+    /**
+     * @param background the background to set
+     */
+    public void setBackground(String background) {
+        this.background = background;
+    }
+    
+    @Override
+    public String toString(){
+        return getName();
+    }
+}
