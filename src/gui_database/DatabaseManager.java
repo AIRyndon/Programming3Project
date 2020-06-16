ac93e12c44fc11b654c94e2c7f7232afec4eb2d7
Taylor <phuongthao234567@gmail.com>
2020-06-16 21:57:11 +1200

    moved lib Derby to the project and initialised 2 tables

---
+++ src/gui_database/DatabaseManager.java
@@ -0,0 +1,96 @@
+package gui_database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+    Here, database is used to store player information and hints that the player grab.
+*/
+public class DatabaseManager 
+{
+    public Connection connection;
+    public Statement statement;
+    public ResultSet resultSet;
+    public ResultSetMetaData resultSetMetaData;
+    
+    protected String tableName;
+    private static final String USER_NAME = "group4";
+    private static final String PASSWORD = "group4";
+    private static final String URL = "jdbc:derby://localhost:1527/RPGDetective;create=true";
+    
+    public DatabaseManager()
+    {
+        establishConnection();
+    }
+    
+    public void establishConnection()
+    {
+        if(connection == null)
+        {
+            try
+            {
+                connection = DriverManager.getConnection(getURL(), getUSER_NAME(), getPASSWORD());
+            }
+            catch (SQLException ex)
+            {
+                System.out.println("SQL exception");
+            }
+        }
+    }
+    
+    public Connection getConnection()
+    {
+        return this.connection;
+    }
+    
+    public void printData()
+    {
+        statement = null;
+        resultSet = null;
+        
+        try 
+        {            
+            statement = connection.createStatement();
+            
+            resultSet = statement.executeQuery("SELECT * FROM " + tableName);
+            resultSetMetaData = resultSet.getMetaData();
+            int columnsNumber = resultSetMetaData.getColumnCount();
+            
+            while (resultSet.next()) 
+            {
+                for (int i = 1; i <= columnsNumber; i++) 
+                {
+                    if (i > 1) System.out.print(",  ");
+                    
+                    System.out.print(resultSet.getString(i));
+                }
+            }
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(HintDatabase.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //Getters
+    public static String getUSER_NAME()
+    {
+        return USER_NAME;
+    }
+    
+    public static String getPASSWORD()
+    {
+        return PASSWORD;
+    }
+    
+    public static String getURL() 
+    {
+        return URL;
+    }
+}
---
+++ src/gui_database/PlayerDatabase.java
@@ -0,0 +1,44 @@
+package gui_database;
+
+import static gui_database.DatabaseManager.getPASSWORD;
+import static gui_database.DatabaseManager.getURL;
+import static gui_database.DatabaseManager.getUSER_NAME;
+import java.sql.DriverManager;
+
+/**
+    * I planned to have 3 tables: Hint (5 hints), Player (all players), 
+    * and Hint_grab (composite entity - records all hints 
+    * the player grabbed, will be reseted every time when the game ends);
+    * 
+    * However, my mind is lagged, so I just created 2 tables first: 
+    * Player and Hint (Hint works as Hint_grab), we do not have any table 
+    * that holds the five hints at the beginning.
+*/
+public class PlayerDatabase extends DatabaseManager
+{
+    private  static int PLAYER_ID = 1;
+    
+    public PlayerDatabase()
+    {
+        super();
+        tableName = "PLAYER";
+    }
+    
+    public void inputDataRow(String player_name, int player_age, char player_gender)
+    {
+        try
+        {
+            connection = DriverManager.getConnection(getURL(), getUSER_NAME(), getPASSWORD());
+            statement = connection.createStatement();
+            
+            statement.executeUpdate("INSERT INTO PLAYER VALUES (" + PLAYER_ID + ", \'" + 
+                    player_name + "\', " + player_age + ", \'" + player_gender + "\')");
+            
+            PLAYER_ID++;
+        }
+        catch (Throwable e)
+        {
+            System.err.println("SQL INSERT exception at " + PLAYER_ID + " - " + e.getMessage());
+        }
+    }
+}
